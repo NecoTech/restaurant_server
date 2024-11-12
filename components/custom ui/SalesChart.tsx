650ccd093fcbcd5b4bc0a97f6d9ded216c1d6d74
njohny <77339291+njohnybroad@users.noreply.github.com>
2024-11-12 14:23:42 -0500

    changes

--- components/custom ui/SalesChart.tsx
+++ components/custom ui/SalesChart.tsx
@@ -5,7 +5,7 @@ import { ResponsiveContainer, LineChart, Line, XAxis, YAxis, CartesianGrid, Tool
 const SalesChart = ({ data }: { data: any[] }) => {
   return (
     <ResponsiveContainer width="100%" height={300}>
-      <LineChart className='w-full h-full' data={data} margin={{ top: 5, right:20, bottom: 5, left:0 }}>
+      <LineChart className='w-full h-full' data={data} margin={{ top: 5, right: 20, bottom: 5, left: 0 }}>
         <Line type="monotone" dataKey="sales" stroke="#8884d8" />
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
         <XAxis dataKey="name" />
