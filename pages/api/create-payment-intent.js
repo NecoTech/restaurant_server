1c1f82d36ad80af57cb3d32441af8ce4a3c4fec2
njohny <77339291+njohnybroad@users.noreply.github.com>
2024-12-09 09:57:42 -0500

    stripe changes

--- pages/api/create-payment-intent.js
+++ pages/api/create-payment-intent.js
@@ -1,4 +1,5 @@
-const Stripe = require('stripe');
+// const Stripe = require('stripe');
+import Stripe from 'stripe';
 
 // Initialize Stripe with your secret key
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
